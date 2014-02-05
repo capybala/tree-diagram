3029d88c4bfab64dde7ad2f8cfa8b4f812e320db
orangain <orangain@gmail.com>
2014-02-05 22:56:54 +0900

    Add tests for toTreeText

--- test/test.js
+++ test/test.js
@@ -2,80 +2,113 @@
 
 var assert = require('assert');
 
+var TreeDiagram = require('../tree-diagram.js').TreeDiagram;
 
-var treeDiagram = require('../tree-diagram.js');
-var TreeDiagram = treeDiagram.TreeDiagram;
-
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3].indexOf(5));
-      assert.equal(-1, [1, 2, 3].indexOf(0));
-    });
-  });
-});
-
-describe('tab_text_to_tree', function() {
-  it('should parse simple indent', function() {
-    assert.deepEqual({
-      'children': [{
-        'text': 'りんご',
+describe('TreeDiagram', function() {
+  describe('#parseIndentText', function() {
+    it('should parse simple indent', function() {
+      assert.deepEqual({
         'children': [{
+          'text': 'りんご',
+          'children': [{
+            'text': 'みかん',
+            'children': [{
+              'text': 'いちご',
+              'children': []
+            }]
+          }]
+        }]
+      },
+      TreeDiagram.parseIndentText(
+        'りんご\n' +
+        '\tみかん\n' +
+        '\t  いちご'
+      ));
+    });
+
+    it('should parse same indent', function() {
+      assert.deepEqual({
+        'children': [{
+          'text': 'りんご',
+          'children': []
+        },
+        {
+          'text': 'いちご',
+          'children': []
+        },
+        {
           'text': 'みかん',
+          'children': []
+        }]
+      },
+      TreeDiagram.parseIndentText(
+        'りんご\n' +
+        'いちご\n' +
+        'みかん'
+      ));
+    });
+
+    it('should parse de-indent', function() {
+      assert.deepEqual({
+        'children': [{
+          'text': 'りんご',
           'children': [{
             'text': 'いちご',
             'children': []
           }]
-        }]
-      }]
-    },
-    TreeDiagram.parseIndentText(
-      'りんご\n' +
-      '\tみかん\n' +
-      '\t  いちご'
-    ));
-  });
-
-  it('should parse same indent', function() {
-    assert.deepEqual({
-      'children': [{
-        'text': 'りんご',
-        'children': []
-      },
-      {
-        'text': 'いちご',
-        'children': []
-      },
-      {
-        'text': 'みかん',
-        'children': []
-      }]
-    },
-    TreeDiagram.parseIndentText(
-      'りんご\n' +
-      'いちご\n' +
-      'みかん'
-    ));
-  });
-
-  it('should parse de-indent', function() {
-    assert.deepEqual({
-      'children': [{
-        'text': 'りんご',
-        'children': [{
-          'text': 'いちご',
+        },
+        {
+          'text': 'みかん',
           'children': []
         }]
       },
-      {
-        'text': 'みかん',
-        'children': []
-      }]
-    },
-    TreeDiagram.parseIndentText(
-      'りんご\n' +
-      '　いちご\n' +
-      'みかん'
-    ));
+      TreeDiagram.parseIndentText(
+        'りんご\n' +
+        '　いちご\n' +
+        'みかん'
+      ));
+    });
+  });
+
+  describe('#toTreeText', function() {
+    it('should stringify simple tree', function() {
+      assert.equal(
+        TreeDiagram.toTreeText({
+          'children': [{
+            'text': 'りんご',
+            'children': [{
+              'text': 'みかん',
+              'children': [{
+                'text': 'いちご',
+                'children': []
+              }]
+            }]
+          }]
+        }),
+        '└─りんご\n' +
+        '　　└─みかん\n' +
+        '　　　　└─いちご'
+      );
+    });
+
+    it('should stringify simple tree without root edge', function() {
+      assert.equal(
+        TreeDiagram.toTreeText({
+          'children': [{
+            'text': 'りんご',
+            'children': [{
+              'text': 'みかん',
+              'children': [{
+                'text': 'いちご',
+                'children': []
+              }]
+            }]
+          }]
+        }, true),
+        'りんご\n' +
+        '└─みかん\n' +
+        '　　└─いちご'
+      );
+    });
   });
 });
--- tree-diagram.js
+++ tree-diagram.js
@@ -2,9 +2,9 @@
 
 var TreeDiagram = {
 
-  indentTextToTreeText: function(indentText) {
+  indentTextToTreeText: function(indentText, stripRootEdges) {
     var tree = TreeDiagram.parseIndentText(indentText);
-    var treeText = TreeDiagram.toTreeText(tree, false);
+    var treeText = TreeDiagram.toTreeText(tree, stripRootEdges);
     return treeText;
   },
 
@@ -43,118 +43,49 @@ var TreeDiagram = {
     return tree;
   },
 
-  toTreeText: function(tree, trim_root_edges) {
-    return tree_to_tree_text(tree, trim_root_edges);
+  toTreeText: function(tree, stripRootEdges) {
+    var treeTextLines = [];
+
+    var nodeToTreeText = function(node, prefix) {
+      for (var i = 0; i < node.children.length; i++) {
+        var child = node.children[i];
+
+        var childPrefix;
+
+        if (child.text === '') {
+          childPrefix = prefix + '│　';
+        } else if (i == node.children.length - 1) {
+          childPrefix = prefix + '└─';
+        } else {
+          childPrefix = prefix + '├─';
+        }
+
+        if (stripRootEdges) {
+          childPrefix = childPrefix.substr(2);
+        }
+        treeTextLines.push(childPrefix + child.text);
+
+        if (child.children.length > 0) {
+          var newPrefix;
+          if (i == node.children.length - 1) {
+            newPrefix = prefix + '　　';
+          } else {
+            newPrefix = prefix + '│　';
+          }
+          nodeToTreeText(child, newPrefix);
+        }
+      }
+    };
+
+    nodeToTreeText(tree, '');
+
+    return treeTextLines.join('\n');
   }
 
 };
 
-function transform(text, trim_root_edges) {
-  var tree = tab_text_to_tree(text);
-  var tree_text = tree_to_tree_text(tree, trim_root_edges);
-
-  return tree_text;
-}
-
-function tab_text_to_tree(tab_text) {
-  var lines = tab_text.split('\n');
-
-  var items = [];
-  var i;
-
-  for (i = 0; i < lines.length; i++) {
-    var line = lines[i];
-
-    var match = line.match(/^(\t*)(.*)$/);
-    var tabs = match[1];
-    var text = match[2];
-    var indent = text === '' ? 0 : tabs.length;
-
-    items.push({'indent': indent, 'text': text});
-  }
-
-  var prev_indent = -1;
-  for (i = lines.length - 1; i >= 0; i--) {
-    var item = items[i];
-    if (item.text === '') {
-      if (prev_indent == -1) {
-        lines.pop();
-      } else {
-        item.indent = prev_indent;
-      }
-    } else {
-      prev_indent = item.indent;
-    }
-  }
-
-
-  var tree = {'depth': 0, 'children': []};
-  var parent = tree;
-  var prev_depth = 1;
-  var prev = null;
-
-  for (i = 0; i < lines.length; i++) {
-    item = items[i];
-
-    var depth = item.indent + 1;
-    var node = {'depth': depth, 'text': item.text, 'children': []};
-
-    if (depth == prev_depth) {
-      // do nothing
-    } else if (depth > prev_depth) {
-      parent = prev;
-    } else if (depth < prev_depth) {
-      while (parent.depth >= depth) {
-        parent = parent.parent;
-      }
-    }
-
-    node.parent = parent;
-    parent.children.push(node);
-    prev = node;
-    prev_depth = depth;
-  }
-
-  return tree;
-}
-
-function tree_to_tree_text(tree, trim_root_edges) {
-  var tree_text = [];
-
-  var node_to_tree_text = function(node, prefix) {
-    for (var i = 0; i < node.children.length; i++) {
-      var child = node.children[i];
-
-      var child_prefix;
-
-      if (child.text === '') {
-        child_prefix = prefix + '│　';
-      } else if (i == node.children.length - 1) {
-        child_prefix = prefix + '└─';
-      } else {
-        child_prefix = prefix + '├─';
-      }
-
-      if (trim_root_edges) {
-        child_prefix = child_prefix.substr(2);
-      }
-      tree_text.push(child_prefix + child.text);
-
-      if (child.children.length > 0) {
-        var new_prefix = i == node.children.length - 1 ? (prefix + '　　') : (prefix + '│　');
-        node_to_tree_text(child, new_prefix);
-      }
-    }
-  };
-
-  node_to_tree_text(tree, '');
-
-  return tree_text.join('\n');
-}
-
 if (typeof module !== 'undefined') {
   module.exports = {
-    'TreeDiagram': TreeDiagram,
-    'tab_text_to_tree': tab_text_to_tree
+    'TreeDiagram': TreeDiagram
   };
 }
