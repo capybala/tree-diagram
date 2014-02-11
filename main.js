08e3ead494a828fe4c9d255ab1a6dc2c2525adf0
orangain <orangain@gmail.com>
2014-02-11 18:59:33 +0900

    Support auto indent and inserting tab

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 
 (function($) {
 
-  var prevText = null;
   var convert = function() {
     var text = srcTextarea.val().replace(/^[\s　]+/, '').replace(/[\s　]+$/, '');
     if (text === prevText) {
@@ -12,10 +11,43 @@
     prevText = text;
   };
 
+  var onTab = function($textarea, e) {
+    $textarea.selection('replace', {text: '\t', caret: 'end'});
+    e.preventDefault();
+  };
+
+  var onEnter = function($textarea, e) {
+    var text = $textarea.val();
+    var caretPos = $textarea.selection('getPos').start;
+
+    // When the caret is at the tail of line, text.charAt(caretPos) == '\n'
+    // So start finding '\n' from caretPos - 1
+    // + 1 returns right index of '\n' when found, or returns 0 when not found
+    var startLinePos = text.lastIndexOf('\n', caretPos - 1) + 1;
+    var line = text.substr(startLinePos, caretPos - startLinePos);
+
+    var match = line.match(/^([ \t　]*)/);
+    var tabs = match[1];
+
+    $textarea.selection('replace', {text: '\n' + tabs, caret: 'end'});
+    e.preventDefault();
+  };
+
+  var prevText = null;
   var srcTextarea = $('#src_text');
   var destTextarea = $('#dest_text');
 
   srcTextarea.keyup(convert);
+  srcTextarea.keydown(function(e) {
+    if (e.which == 9) {
+      // Tab
+      onTab($(this), e);
+    } else if (e.which == 13) {
+      // Enter
+      onEnter($(this), e);
+    }
+  });
+
   destTextarea.focus(function() {
     $(this).select();
     // for Chrome, see: http://stackoverflow.com/questions/5797539/jquery-select-all-text-from-a-textarea
@@ -25,8 +57,8 @@
   });
 
   srcTextarea.val('好きな果物\n' +
-                  '　りんご\n' +
-                  '　みかん');
+                  '\tりんご\n' +
+                  '\tみかん');
   convert();
 
 })(jQuery);
