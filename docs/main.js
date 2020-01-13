a511d1b86742d4e16b272e597c12da850165db82
orangain <orangain@gmail.com>
2020-01-13 18:07:26 +0900

    Auto format

--- docs/main.js
+++ docs/main.js
@@ -1,9 +1,11 @@
-'use strict';
+"use strict";
 
 (function($) {
-
   var convert = function() {
-    var text = srcTextarea.val().replace(/^[\s　]+/, '').replace(/[\s　]+$/, '');
+    var text = srcTextarea
+      .val()
+      .replace(/^[\s　]+/, "")
+      .replace(/[\s　]+$/, "");
     if (text === prevText) {
       return;
     }
@@ -12,30 +14,30 @@
   };
 
   var onTab = function($textarea, e) {
-    $textarea.selection('replace', {text: '\t', caret: 'end'});
+    $textarea.selection("replace", { text: "\t", caret: "end" });
     e.preventDefault();
   };
 
   var onEnter = function($textarea, e) {
     var text = $textarea.val();
-    var caretPos = $textarea.selection('getPos').start;
+    var caretPos = $textarea.selection("getPos").start;
 
     // When the caret is at the tail of line, text.charAt(caretPos) == '\n'
     // So start finding '\n' from caretPos - 1
     // + 1 returns right index of '\n' when found, or returns 0 when not found
-    var startLinePos = text.lastIndexOf('\n', caretPos - 1) + 1;
+    var startLinePos = text.lastIndexOf("\n", caretPos - 1) + 1;
     var line = text.substr(startLinePos, caretPos - startLinePos);
 
     var match = line.match(/^([ \t　]*)/);
     var tabs = match[1];
 
-    $textarea.selection('replace', {text: '\n' + tabs, caret: 'end'});
+    $textarea.selection("replace", { text: "\n" + tabs, caret: "end" });
     e.preventDefault();
   };
 
   var prevText = null;
-  var srcTextarea = $('#src_text');
-  var destTextarea = $('#dest_text');
+  var srcTextarea = $("#src_text");
+  var destTextarea = $("#dest_text");
 
   srcTextarea.keyup(convert);
   srcTextarea.keydown(function(e) {
@@ -51,17 +53,18 @@
   destTextarea.focus(function() {
     $(this).select();
     // for Chrome, see: http://stackoverflow.com/questions/5797539/jquery-select-all-text-from-a-textarea
-    $(this).one('mouseup', function(e) {
+    $(this).one("mouseup", function(e) {
       e.preventDefault();
     });
   });
 
-  srcTextarea.val('上のテキストエリアに\n' +
-                  '\tタブや\n' +
-                  '\t\tスペースで\n' +
-                  '\t整形したテキストを書くと、\n' +
-                  '\t\t下のテキストエリアに\n' +
-                  '\t\t樹形図が表示されます。');
+  srcTextarea.val(
+    "上のテキストエリアに\n" +
+      "\tタブや\n" +
+      "\t\tスペースで\n" +
+      "\t整形したテキストを書くと、\n" +
+      "\t\t下のテキストエリアに\n" +
+      "\t\t樹形図が表示されます。"
+  );
   convert();
-
 })(jQuery);
